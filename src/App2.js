55d84bbe0852911cf1b0558c7402e919533f4340
Coboy <416212316@qq.com>
2024-03-22 22:40:15 +0800

    mini-Mobx run in react

---
+++ src/App2.js
@@ -0,0 +1,28 @@
+import { reactive, shallowRef } from "@vue/reactivity";
+import { observer } from "./vue-reactivity-react"
+
+class DataService {
+  constructor(val) {
+    this.r = shallowRef(val)
+  }
+  get count() {
+    return this.r.value
+  }
+  setCount(val) {
+    this.r.value = val
+  }
+}
+const dataService = new DataService(0)
+const TimerView = observer(({ proxy }) => <span>the content run in @vue/reactivity is "Seconds passed: {proxy.count}"</span>)
+
+function App() {
+  return (
+    <TimerView proxy={dataService}></TimerView>
+  );
+}
+
+setInterval(() => {
+  dataService.r.value = Date.now()
+}, 1000)
+
+export default App;
